693eba6e6d653493fe35c75d3d7476a36f4f0dca
Nalipp <nateplipp@gmail.com>
2015-12-21 17:38:12 +0900

    add cities

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,10 +3,19 @@ class JobsController < ApplicationController
   def index
     if params[:q].present?
       @jobs = IndeedAPI.search_jobs(q: params[:q], l: params[:l])
-      #@results = @jobs.total_results
+      @jobs_san_francisco  = IndeedAPI.search_jobs(q: params[:q], l: 'san francisco')
+      @jobs_denver         = IndeedAPI.search_jobs(q: params[:q], l: 'denver')
+      @jobs_seattle        = IndeedAPI.search_jobs(q: params[:q], l: 'seattle')
+      @jobs_new_york       = IndeedAPI.search_jobs(q: params[:q], l: 'new york')
+      @jobs_chicago        = IndeedAPI.search_jobs(q: params[:q], l: 'chicago')
+      @jobs_los_angeles    = IndeedAPI.search_jobs(q: params[:q], l: 'los angeles')
+      @jobs_portland       = IndeedAPI.search_jobs(q: params[:q], l: 'portland')
+      @jobs_austin         = IndeedAPI.search_jobs(q: params[:q], l: 'austin')
+      @jobs_washington_dc  = IndeedAPI.search_jobs(q: params[:q], l: 'washington dc')
+      @jobs_salt_lake_city = IndeedAPI.search_jobs(q: params[:q], l: 'salt lake city')
+      @jobs_boston         = IndeedAPI.search_jobs(q: params[:q], l: 'boston')
     else
-      #@jobs = []
-      #@results = []
+      render 'index'
     end
   end
 
